ccc57e23d5f118b848c43637633aed8161e7a9a9
Gabriella Broussard <gabriellabroussard@Gabriellas-MacBook-Air.local>
2020-07-21 09:33:48 -0500

    corrections

--- select_exercises.sql
+++ select_exercises.sql
@@ -3,20 +3,24 @@ SELECT database ();
 SHOW tables;
 DESCRIBE albums;
 # The name of all albums by Pink Floyd
-SELECT * FROM albums WHERE artist = 'Pink Floyd';
+SELECT name as 'Pink Floyd Albums'
+FROM albums WHERE artist = 'Pink Floyd';
 # The year Sgt. Pepper's Lonely Hearts Club Band was released
-SELECT release_date FROM albums
+SELECT release_date as 'Year Released'
+FROM albums
 WHERE name LIKE 'Sgt%';
 # The genre for the album Nevermind
 SELECT genre FROM albums  WHERE name LIKE 'NEVER%';
 # Which albums were released in the 1990s
-SELECT * FROM albums 
-WHERE release_date > '1990'
-AND release_date < '2000';
+SELECT * FROM albums
+WHERE release_date >= '1990'
+AND release_date <= '1999';
 # Which albums had less than 20 million certified sales
 SELECT * FROM albums
-where sales < '20.0'
+WHERE sales < '20.0';
 # All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
 SELECT * FROM albums 
-where genre = 'rock';
-#These results do not include hard rock or progressive rock because they are their own queries.
+WHERE genre = 'rock';
+#These results do not include hard rock or progressive rock because they are their own queries. Therefore a wildcard is needed to find all types of rock in one query.
+SELECT * FROM albums
+WHERE genre LIKE '%rock%';
